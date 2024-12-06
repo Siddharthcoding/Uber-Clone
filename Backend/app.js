d89a58b16b6cdb92867fbac740bdaaea8ce8dc07
Siddharthcoding <siddharthkumarmishra1011@gmail.com>
2024-12-07 00:39:04 +0530

    User registration backend

--- Backend/app.js
+++ Backend/app.js
@@ -3,13 +3,22 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
+import connectToDb from "./db/db.js";
+import userRoutes from "./routes/user.routes.js";
+// const userRoutes = require("./routes/user.routes");
+
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+connectToDb();
 app.use(cors());   // for now it accepts request from every domain
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
+
+app.use('/users', userRoutes);
 
 export default app;
---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { body } from "express-validator";
+import registerUser from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.post('/register', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname').isLength({min: 3}).withMessage
+    ('First name must be atleast 3 characters long'),
+    body('password').isLength({min: 6}).withMessage
+    ('Password name must be atleast 6 characters long'),
+],
+    registerUser // action performed if validation check go wrong
+)
+
+export default router;
