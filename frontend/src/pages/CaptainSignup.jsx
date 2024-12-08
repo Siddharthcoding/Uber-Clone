31aaea35f057fcf7abc861052fad961e3f4f851c
Siddharthcoding <siddharthkumarmishra1011@gmail.com>
2024-12-08 17:39:40 +0530

    login and signup pages for user and captain

--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -1,8 +1,96 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
 
 function CaptainSignup() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [userData, setUserData] = useState({});
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setUserData({
+            fullName: {
+                firstName: firstName,
+                lastName: lastName,
+            },
+            email: email,
+            password: password
+        })
+
+        setEmail('');
+        setPassword('');
+        setFirstName('');
+        setLastName('');
+    }
     return (
-        <div className="div">Captain Signup</div>
+        <div>
+            <div className='py-5 px-5 p-7 h-screen flex flex-col justify-between'>
+            <div>
+                <img className='w-16 mb-10' src="https://www.svgrepo.com/show/505031/uber-driver.svg" alt="" />
+                <form onSubmit={(e) => {
+                    submitHandler(e);
+                }}>
+                    <h3 className='text-base w-full font-medium mb-2'>What's our Captain's name</h3>
+                    <div className='flex gap-4 mb-6'>
+                        <input 
+                            required
+                            value={firstName}
+                            onChange={(e)=>{
+                                setFirstName(e.target.value);
+                            }}
+                            className='bg-[#eeeeee] w-1/2 rounded px-4 py-2 border text-base placeholder::text-sm'
+                            type="text" 
+                            placeholder='First name'
+                        />
+                        <input 
+                            required
+                            value={lastName}
+                            onChange={(e)=>{
+                                setLastName(e.target.value);
+                            }}
+                            className='bg-[#eeeeee] w-1/2 rounded px-4 py-2 border text-base placeholder::text-sm'
+                            type="text" 
+                            placeholder='Last name'
+                        />
+                    </div>
+                    <h3 className='text-base font-medium mb-2'>What's our Captain's email</h3>
+                    <input 
+                        required
+                        value={email}
+                            onChange={(e)=>{
+                                setEmail(e.target.value);
+                            }}
+                        className='bg-[#eeeeee] mb-6 rounded px-4 py-2 border w-full text-base placeholder::text-sm'
+                        type="email" 
+                        placeholder='email@example.com'
+                    />
+                    <h3 className='text-base font-medium mb-2'>Enter Password</h3>
+                    <input 
+                        required
+                        value={password}
+                            onChange={(e)=>{
+                                setPassword(e.target.value);
+                            }}
+                        className='bg-[#eeeeee] mb-6 rounded px-4 py-2 border w-full text-base placeholder::text-sm'
+                        type="password" 
+                        placeholder='password'
+                    />
+                    <button
+                        className='bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-base placeholder::text-sm'
+                    >Sign in</button>
+                </form>
+                <p className='text-center'>Already have a account? <Link to='/captain-login' className='text-blue-600'>Login here</Link></p>
+            </div>
+            <div>
+            <p className='text-[10px] leading-tight'>This site is protected by reCAPTCHA and the <span className='underline'>Google Privacy Policy </span>
+                     and <span className='underline'>Terms of Service apply</span>.
+                </p>
+            </div>
+            </div>
+        </div>
     )
 }
 
